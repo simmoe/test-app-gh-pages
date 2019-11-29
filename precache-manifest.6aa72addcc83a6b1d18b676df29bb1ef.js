db1bfe01a9ccded6b17d276a7f6a507749722217
Simon Moe <simmoe@gmail.com>
2019-11-29 09:43:53 +0100

    Updates

---
+++ precache-manifest.6aa72addcc83a6b1d18b676df29bb1ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ae100b44d17ca4414dd848b725bb968",
+    "url": "/test-app-gh-pages/index.html"
+  },
+  {
+    "revision": "127ed28d12b941cd229d",
+    "url": "/test-app-gh-pages/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "781a8937d943bd82aac3",
+    "url": "/test-app-gh-pages/static/js/2.c05fa0b6.chunk.js"
+  },
+  {
+    "revision": "127ed28d12b941cd229d",
+    "url": "/test-app-gh-pages/static/js/main.9bfec059.chunk.js"
+  },
+  {
+    "revision": "c9d5cc951014808360b6",
+    "url": "/test-app-gh-pages/static/js/runtime-main.d841b354.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/test-app-gh-pages/static/media/logo.25bf045c.svg"
+  }
+]);
